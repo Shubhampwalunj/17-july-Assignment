5139f90c9ec92d5e2e6d6fb4ff1177faf3f4c6b1
Shubhampwalunj <86958175+Shubhampwalunj@users.noreply.github.com>
2022-08-17 21:50:07 +0530

    Add files via upload

---
+++ Assignment 1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+main() {
+	cout << "*\n**\n***\n****";
+	return 0;
+}
+
---
+++ Assignment 17 aug.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+main() {
+	cout << "Name- Shubham Walunj\nAge-23";
+		return 0;
+}
+	
